e8400f39de451d26d8b15b6648d2077795fc741d
Enrique Raúl OR <128866748+enriraul@users.noreply.github.com>
2025-06-03 22:01:11 -0600

    Add files via upload

---
+++ EscuelaDeCodigo/Modulo1_FundamentosDeProgramacion/Sesion2/introduccionPython.py
@@ -0,0 +1,40 @@
+# Formas de definir una variable
+# 
+
+# Puede iniciar 
+#  _, letra mayuscula o minuscula
+# no debe exitir espacios, pero si podemos 
+# separarlas _ 
+
+#variables validas
+#_nombre_123
+#_clave
+#_usuario
+#edadUsuario
+#deporte
+
+# Enteros int 1,2,3,4,5,7,8,...
+# Flotantes float 1.5,1.8, 2.333, 5.0
+# Booleanos boolean true, false
+# caracteres char 'c', 'a', 'b',
+# cadena  str "Enrique", "abecedario", "Enrique Raul"
+
+#Tipo de datos
+
+entero = 10
+flotante = 3.14156
+cadena = "Esto es una cadena"
+boolean = True
+null = None
+nombre = "Pepe"
+
+#impresión a pantalla
+print("Bienvenidos al curso")
+print(nombre)
+print(entero)
+
+#Solicitar información al usuario
+nombre = input("Dime tu nombre ")
+print(nombre)
+edad = int(input("Cuantos años tienes"))   #int()
+print(edad,type(edad))
